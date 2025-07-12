31c0a1a2e41b17d6e4540f35a6f2bf7a9f09398f
Nazish-Kiran <nazishkirancosmos123@gmail.com>
2025-07-12 00:51:01 -0700

    added navbar and fonts

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+
+const page = () => {
+  return (
+    <>
+    <div className="h-screen w-full bg-[#f1f1f1]">
+      <Navbar/>
+    </div>
+    </>
+  )
+}
+
+export default page
